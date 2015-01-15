7c24e96f3edee6538a7579cafca9bc6813e7ed46
Marcel Corso <marcel.corso@gmail.com>
2015-01-15 19:02:54 +0100

    convert midi to json

---
+++ convert.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var midiConverter = require('midi-converter');
+
+for (i = 1; i < 55; i++) { 
+  var midiSong = fs.readFileSync('midi/' + i + '.mid', 'binary');
+  var jsonSong = midiConverter.midiToJson(midiSong);
+  fs.writeFileSync('app/assets/midi/' + i + '.json', JSON.stringify(jsonSong));
+}
+
+
